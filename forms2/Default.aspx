85b49c35a50bc78b061824a30f5eac3f92bb0194
Sebasprzd <seperazade@est.utn.ac.cr>
2024-07-21 22:58:38 -0600

    Initial commit

---
+++ forms2/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Formulario de Registro</title>
+    <telerik:RadStyleSheetManager ID="RadStyleSheetManager1" runat="server" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+            <Scripts>
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+            </Scripts>
+        </telerik:RadScriptManager>
+        <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+        </telerik:RadAjaxManager>
+        <div>
+            <telerik:RadTextBox ID="RadTextBoxNombre" runat="server" Label="Nombre:" Width="200px"></telerik:RadTextBox><br />
+            <telerik:RadTextBox ID="RadTextBoxApellido" runat="server" Label="Apellido:" Width="200px"></telerik:RadTextBox><br />
+            <telerik:RadTextBox ID="RadTextBoxCorreo" runat="server" Label="Correo:" Width="200px"></telerik:RadTextBox><br />
+            <telerik:RadButton ID="RadButtonEnviar" runat="server" Text="Enviar" OnClick="RadButtonEnviar_Click"></telerik:RadButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ forms2/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using Telerik.Web.UI;
+
+public partial class Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Código para ejecutar en cada carga de página (si es necesario)
+    }
+
+    protected void RadButtonEnviar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            // Obtener la cadena de conexión desde el archivo de configuración
+            string connectionString = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+
+            // Crear una conexión con la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open(); // Abrir la conexión
+
+                // Definir la consulta SQL para insertar datos
+                string query = "INSERT INTO registro (Nombre, Apellido, Correo) VALUES (@Nombre, @Apellido, @Correo)";
+
+                // Crear un comando SQL y asignarle la consulta y la conexión
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Agregar los parámetros y sus valores
+                    command.Parameters.AddWithValue("@Nombre", RadTextBoxNombre.Text);
+                    command.Parameters.AddWithValue("@Apellido", RadTextBoxApellido.Text);
+                    command.Parameters.AddWithValue("@Correo", RadTextBoxCorreo.Text);
+
+                    // Ejecutar la consulta
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // Manejo de errores
+            Response.Write("<script>alert('Error al procesar la solicitud: " + ex.Message + "');</script>");
+        }
+    }
+}
